f510b739ed300bb45efd0b3dfa0f6ff7f57befdc
Abraham Ortega <AbrahamOrtegaM@outlook.com>
2025-01-06 13:13:08 -0600

    git init

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Time Tracking Dashboard</title>
       <Head />
+
       <body className="antialiased">
         <Main />
         <NextScript />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+
+        // Primary
+
+        blue: "#5847eb",
+        lightOrange: "#ff8c66", //Work
+        softBlue: "#56c2e6", //Play
+        lightRed: "#ff5c7c", //Study
+        limeGreen: "#4acf81", //Exercise
+        violet: "#7536d3", //Social
+        softYellow: "#f1c65b", //Self Care
+
+        // Neutral
+        veryDarkBlue: "#0f1424",
+        darkBlue: "#1c1f4a",
+        desaturatedBlue: "#6f76c8",
+        paleBlue: "#bdc1ff",
       },
     },
   },
